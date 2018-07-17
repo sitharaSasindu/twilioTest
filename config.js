8622a5e831194734c65a97cfa0b68b6471538170
sitharaSasindu <sasindusithara@gmail.com>
2018-07-17 16:10:08 +0530

    t

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: '3489492',
+  apiSecret: 'dfd0119bc7ea365ecef9a5a58d9c88760ec7fb58',
+  sip: {
+    uri: 'breakouttokbox',
+    username: 'twilioauth',
+    password: 'Innovationfoundry@123',
+  },
+  callerId: 'authtokbox',
+};
